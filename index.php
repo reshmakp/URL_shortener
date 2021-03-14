06105de24bd6f762a2481e272312ae96aa36ad15
reshmakp <40451766+reshmakp@users.noreply.github.com>
2021-03-14 17:34:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ $servername = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = 'shorten_db';
-$base_url='http://localhost/url/'; 
 if(isset($_GET['url']) && $_GET['url']!="")
 { 
 $url=urldecode($_GET['url']);
